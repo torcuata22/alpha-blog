75dc17ae04ffba68747baa9cebd2d065bdfeef05
torcuata22 <marilynjmarquez@gmail.com>
2023-08-17 12:43:38 -0400

    new, update, edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,30 @@ class ArticlesController < ApplicationController
       @article = Article.new
     end
 
+    def edit
+     # binding.break this is the debugger
+      @article = Article.find(params[:id])
+    end
+
     def create
-      render plain: params[:article]  
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      if @article.save
+        flash[:notice] = "Your article was successfully created!"
+        redirect_to @article
+      else   
+        render 'new'
+      end  
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Your article was successfully updated"
+        redirect_to @article
+      else    
+        render 'edit'
+      end
+      
     end
 
 end
