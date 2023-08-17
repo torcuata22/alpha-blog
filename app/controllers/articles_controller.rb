1a26a6aa0bc96e16a4d833635b33a23cc96bee13
torcuata22 <marilynjmarquez@gmail.com>
2023-08-17 14:37:18 -0400

    layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,4 +37,10 @@ class ArticlesController < ApplicationController
       
     end
 
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path, notice: 'Article was successfully destroyed.'
+    end
+
 end
