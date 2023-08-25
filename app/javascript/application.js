de87ab07187efd082d60f6e8f654ee65fcadab59
torcuata22 <marilynjmarquez@gmail.com>
2023-08-25 14:45:27 -0400

    still styling, starting with messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,5 @@ import "@hotwired/turbo-rails";
 import "controllers";
 import "popper";
 import "bootstrap";
+import "trix";
+import "@rails/actiontext";
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  # has_rich_text :description
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
   validates :description, presence: true, length: {minimum: 10, maximum: 500}
 
