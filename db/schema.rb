7cc09ef8f96da8315a9633dc73054bba4bd46bc6
torcuata22 <marilynjmarquez@gmail.com>
2023-08-29 11:40:21 -0400

    users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_25_165020) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_152935) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -52,6 +52,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_25_165020) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
