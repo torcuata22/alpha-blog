8ecc9735081ae137e9ff61afa9aa58c9728324d4
torcuata22 <marilynjmarquez@gmail.com>
2023-08-31 15:44:00 -0400

    updating

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   root "posts#index"
   get "/about", to: "posts#about"
   resources :articles
+  get 'signup', to: 'users#new'
+  #form looks for users path to submit form when completed, so we need this:
+  #post 'users', to: 'users#create'
+  #or I can get all the routes set up automatically (except the signup one I already have):
+  resources :users, except: [:new]
 end
