95cec6b717f907f8f0b5624d52d75f52b8bc5754
torcuata22 <marilynjmarquez@gmail.com>
2023-09-06 15:17:37 -0400

    users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -17,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information has been updated"
-      redirect_to articles_path
+      redirect_to @user 
     else       
       render 'edit'
     end
