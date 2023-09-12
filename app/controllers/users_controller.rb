dad6251c93d73fbd7e8a1e8466ca383dfc6634b7
torcuata22 <marilynjmarquez@gmail.com>
2023-09-12 17:01:48 -0400

    controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+  
   def show
     @articles = @user.articles.paginate(:page => params[:page], :per_page => 5)
   end
@@ -47,6 +49,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
   end
-
+  
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own articles"
+      redirect_to @user
+    end
+  end
 
 end
