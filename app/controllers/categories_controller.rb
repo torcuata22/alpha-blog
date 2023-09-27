04eb15a6a6e7472064f72465383d9e37524f528d
torcuata22 <marilynjmarquez@gmail.com>
2023-09-27 15:39:09 -0400

    integration test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,34 @@
 class CategoriesController < ApplicationController
     def new
-        
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was successfully created"
+            redirect_to @category
+        else       
+            render 'new'
+        end
     end
 
     def index
-        
+       categories = Category.all 
     end
 
     def show
-        
+        @category = Category.find(params[:id])
     end
-     
+    
+    
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+  
+
+
 end
+
