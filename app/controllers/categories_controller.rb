07ca45809e77ddecaf85fc5da68647a1c55e0927
torcuata22 <marilynjmarquez@gmail.com>
2023-10-09 11:58:27 -0400

    categories edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,20 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice] = "Category name updated successfully"
+            redirect_to  @category
+        else 
+            render 'edit'
+        end
+    end
+
     def index
        @categories = Category.paginate(:page => params[:page], :per_page => 5 )
     end
@@ -23,7 +37,7 @@ class CategoriesController < ApplicationController
         @category = Category.find(params[:id])
         @articles = @category.articles.paginate(:page => params[:page], :per_page => 5)
     end
-    
+
     
     private
 
